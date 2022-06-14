b4000ccdf30d4a20cf5524381ba2dfd43473fb72
Noah Palin <noah.m.palin@gmail.com>
2022-06-14 18:27:42 +0000

    Added a name to the snake and the ability to move it a single space.

--- client.js
+++ client.js
@@ -4,20 +4,22 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",// IP address here,
-    port: "50541"// PORT number here,
+    port: "50541",// PORT number here,
   });
 
-  conn.on("data", () => {
-    console.log("you ded cuz you idled");
+  conn.on("data", (data) => {
+    console.log(data);
   });
   
+  // Displays the snake name.
   conn.on("connect", () => {
     console.log("Successfully connected to game server!");
+    conn.write("Name: TBS");
+
+    // The following line moves the snake up.
+    //conn.write("Move up");
   });
 
-  conn.on("connect", () => {
-    console.log("Name: TBS ");
-  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
